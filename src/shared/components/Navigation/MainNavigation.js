a2e8244c54055088688d15961ef4ad50b5c1344f
Jio Wang <jio.wang@outlook.com>
2021-09-20 16:57:46 -0400

    fixed main navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+
+import { AuthContext } from '../../context/auth-context';
 
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+    const auth = useContext(AuthContext);
+
     return (
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top">To-do List</a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div className="container">
+                <NavLink className="navbar-brand" to={auth.isLoggedIn ? "/u1/myLists" : '/home'}>To-do List</NavLink>
+                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
-                    <i class="fas fa-bars"></i>
+                    <i className="fas fa-bars"></i>
                 </button>
             </div>
             <NavLinks />
